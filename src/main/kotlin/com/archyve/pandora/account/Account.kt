94a958668f7d96e3d4e7701488ed2ec0ac7a0dd6
Isaac Dolphin <isaac_dolphin@outlook.com>
2025-03-02 20:50:05 +0000

    Created basic AccountRepository and Account model.

---
+++ src/main/kotlin/com/archyve/pandora/account/Account.kt
@@ -0,0 +1,17 @@
+package com.archyve.pandora.account
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.util.UUID
+
+@Entity
+data class Account(
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    val id: String = UUID.randomUUID().toString(),
+    var firstName: String,
+    var lastName: String,
+    var email: String,
+    var password: String
+)
